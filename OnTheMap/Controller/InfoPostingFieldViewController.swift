852473a08770ae2251155eda3f7478f279229891
Ty Hopp <hopp.ty.c@gmail.com>
2021-12-06 11:58:29 +0800

    Info posting cancel logic

---
+++ OnTheMap/Controller/InfoPostingFieldViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InfoPostingFieldViewController.swift
+//  OnTheMap
+//
+//  Created by Ty Hopp on 6/12/21.
+//
+
+import UIKit
+
+class InfoPostingFieldViewController: UIViewController {
+    
+    // MARK - Action
+    
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+}
